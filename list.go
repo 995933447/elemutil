0a8efc0b5c3870e0e7979cd2e82537185719fde4
995933447 <995933447@example.com>
2025-12-23 23:51:29 +0400

    ok

--- list.go
+++ list.go
@@ -1,5 +1,11 @@
 package elemutil
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
 func InUint32List(val uint32, list []uint32) bool {
 	var exist bool
 	for _, v := range list {
@@ -119,4 +125,195 @@ func InStringList(val string, list []string) bool {
 		}
 	}
 	return exist
+}
+
+func PluckUint64(list interface{}, fieldName string) ([]uint64, error) {
+	var result []uint64
+	vo := reflect.ValueOf(list)
+	switch vo.Kind() {
+	case reflect.Array, reflect.Slice:
+		for i := 0; i < vo.Len(); i++ {
+			elem := vo.Index(i)
+
+			for elem.Kind() == reflect.Ptr {
+				elem = elem.Elem()
+			}
+
+			// 如果某一个元素的nil，跳过从这个元素中获取数据
+			if !elem.IsValid() {
+				continue
+			}
+
+			if elem.Kind() != reflect.Struct {
+				panic("element not struct")
+			}
+
+			f := elem.FieldByName(fieldName)
+			if !f.IsValid() {
+				return nil, fmt.Errorf("struct missed field %s", fieldName)
+			}
+
+			if f.Kind() != reflect.Uint64 {
+				return nil, fmt.Errorf("struct element %s type required uint64", fieldName)
+			}
+
+			result = append(result, f.Uint())
+		}
+	default:
+		return nil, fmt.Errorf("required list of struct type")
+	}
+
+	return result, nil
+}
+
+func PluckInt64(list interface{}, fieldName string) ([]int64, error) {
+	var result []int64
+	vo := reflect.ValueOf(list)
+	switch vo.Kind() {
+	case reflect.Array, reflect.Slice:
+		for i := 0; i < vo.Len(); i++ {
+			elem := vo.Index(i)
+
+			for elem.Kind() == reflect.Ptr {
+				elem = elem.Elem()
+			}
+
+			// 如果某一个元素的nil，跳过从这个元素中获取数据
+			if !elem.IsValid() {
+				continue
+			}
+
+			if elem.Kind() != reflect.Struct {
+				return nil, fmt.Errorf("element not struct")
+			}
+
+			f := elem.FieldByName(fieldName)
+			if !f.IsValid() {
+				return nil, fmt.Errorf("struct missed field %s", fieldName)
+			}
+
+			if f.Kind() != reflect.Int64 {
+				return nil, fmt.Errorf("struct element %s type required int64", fieldName)
+			}
+
+			result = append(result, f.Int())
+		}
+	default:
+		return nil, fmt.Errorf("required list of struct type")
+	}
+
+	return result, nil
+}
+
+func PluckUint64Set(list interface{}, fieldName string) (map[uint64]struct{}, error) {
+	out, err := PluckUint64(list, fieldName)
+	if err != nil {
+		return nil, err
+	}
+	res := map[uint64]struct{}{}
+	for _, v := range out {
+		res[v] = struct{}{}
+	}
+	return res, nil
+}
+
+func PluckUint32(list interface{}, fieldName string) ([]uint32, error) {
+	var result []uint32
+	vo := reflect.ValueOf(list)
+	switch vo.Kind() {
+	case reflect.Array, reflect.Slice:
+		for i := 0; i < vo.Len(); i++ {
+			elem := vo.Index(i)
+
+			for elem.Kind() == reflect.Ptr {
+				elem = elem.Elem()
+			}
+
+			// 如果某一个元素的nil，跳过从这个元素中获取数据
+			if !elem.IsValid() {
+				continue
+			}
+
+			if elem.Kind() != reflect.Struct {
+				return nil, fmt.Errorf("element not struct")
+			}
+
+			f := elem.FieldByName(fieldName)
+			if !f.IsValid() {
+				return nil, fmt.Errorf("struct missed field %s", fieldName)
+			}
+
+			if f.Kind() != reflect.Uint32 {
+				return nil, fmt.Errorf("struct element %s type required uint32", fieldName)
+			}
+			result = append(result, uint32(f.Uint()))
+		}
+	default:
+		return nil, fmt.Errorf("required list of struct type")
+	}
+	return result, nil
+}
+
+func PluckUint32Set(list interface{}, fieldName string) (map[uint32]struct{}, error) {
+	out, err := PluckUint32(list, fieldName)
+	if err != nil {
+		return nil, err
+	}
+	res := map[uint32]struct{}{}
+	for _, v := range out {
+		res[v] = struct{}{}
+	}
+
+	return res, nil
+}
+
+func PluckString(list interface{}, fieldName string) ([]string, error) {
+	var result []string
+	vo := reflect.ValueOf(list)
+	switch vo.Kind() {
+	case reflect.Array, reflect.Slice:
+		for i := 0; i < vo.Len(); i++ {
+			elem := vo.Index(i)
+
+			for elem.Kind() == reflect.Ptr {
+				elem = elem.Elem()
+			}
+
+			// 如果某一个元素的nil，跳过从这个元素中获取数据
+			if !elem.IsValid() {
+				continue
+			}
+
+			if elem.Kind() != reflect.Struct {
+				return nil, errors.New("element not struct")
+			}
+
+			f := elem.FieldByName(fieldName)
+			if !f.IsValid() {
+				return nil, fmt.Errorf("struct missed field %s", fieldName)
+			}
+
+			if f.Kind() != reflect.String {
+				return nil, fmt.Errorf("struct element %s type required string", fieldName)
+			}
+			result = append(result, f.String())
+		}
+	default:
+		return nil, errors.New("required list of struct type")
+	}
+	return result, nil
+}
+
+func PluckStringSet(list interface{}, fieldName string) (map[string]struct{}, error) {
+	out, err := PluckString(list, fieldName)
+	if err != nil {
+		return nil, err
+	}
+
+	res := map[string]struct{}{}
+	for _, v := range out {
+		res[v] = struct{}{}
+	}
+
+	return res, nil
 }
